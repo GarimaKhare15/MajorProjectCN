4473ccd3cad32e7b018b634e2301c1a054b18359
GarimaKhare15 <garimakhare.cse19@chitkarauniversity.edu.in>
2022-02-10 12:27:34 +0530

    set up static files access for layouts and pages

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require('express');
 
 const app = express();
 
+const expressLayouts = require('express-ejs-layouts');
 const port = 8000;
+
+app.use(express.static('./assests'))
+app.use(expressLayouts);
+
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 
 //use express router
 app.use('/',require('./routes'));
 
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err){
         //console.log("Error!",error);
